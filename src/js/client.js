7cd7ee20d9e5f930fa7df9685b5639da91fefd22
Jordan <jordankadish93@gmail.com>
2018-01-17 20:51:47 +0200

    installed node modules and got some basic dev tool stuff done (webpack --watch, webpack-dev-server)

--- src/js/client.js
+++ src/js/client.js
@@ -1,11 +1,15 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import ReactDOM from "react-dom"; // rendering engine, separated to allow syntax versatility
 
-class Layout extends React.Component{
-    render(){
-        return(<h1>it works!</h1>);
+class Layout extends React.Component{ // Everything is a component, similar to everything in HTML being an element (head, body, h1 etc)
+    render(){ // render method is essentially "what are we outputting?"
+        return(
+            <h1>it's working!</h1>
+            // in js, we can do stuff like 'var div = document.createElement("div")'
+            // this allows stuff like "div.innerHTML" for example. But this is effort, so react allows us to type native HTML as above
+        );
     }
 }
 
-const app = document.getElementById('app');
-ReactDOM.render(<Layout/>, app);
+const app = document.getElementById('app'); // we get the 'app' id from the html page
+ReactDOM.render(<Layout/>, app); // here's where the rendering happens. We use the class as an element tag and render it into the page id we got
--- webpack.config.js
+++ webpack.config.js
@@ -5,22 +5,22 @@ var path = require('path');
 module.exports = {
     context: path.join(__dirname, "src"),
     devtool: debug ? "inline-sourcemap" : false,
-    entry: "./js/client.js",
+    entry: "./js/client.js", // taking this in
     module: {
         loaders: [
             {
-                test: /\.jsx?$/,
-                exclude: /(node_modules|bower_components)/,
+                test: /\.jsx?$/,// all React js code should be loaded with the babel loader, which is a transpiler (program that converts code between languages)
+                exclude: /(node_modules|bower_components)/, // do not convert node or bower stuff
                 loader: 'babel-loader',
                 query: {
-                    presets: ['react', 'es2015', 'stage-0'],
+                    presets: ['react', 'es2015', 'stage-0'], // the stuff we;re converting (react jsx, ES6)
                     plugins: ['react-html-attrs', 'transform-decorators-legacy', 'transform-class-properties'],
                 }
             }
         ]
     },
     output: {
-        path: __dirname + "/src/",
+        path: __dirname + "/src/", //output of this code goes into the current directory/source, as client.min.js
         filename: "client.min.js"
     },
     plugins: debug ? [] : [
