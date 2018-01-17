e2d7f773d0f8970e1b5371b5db2c89c7cfb64a9a
Jordan <jordankadish93@gmail.com>
2018-01-17 21:13:55 +0200

    refactoring. Removed components from client.js and began modularity via separate components in the components folder

--- src/js/client.js
+++ src/js/client.js
@@ -1,28 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom"; // rendering engine, separated to allow syntax versatility
 
-class Layout extends React.Component{ // Everything is a component, similar to everything in HTML being an element (head, body, h1 etc)
-    
-    // Here, we're gonna type some Javascript code
-    constructor(){// js has constructor methods!
-        super(); // because we're inside a component, we must always super() a constructor (inheritance basics)
-        this.bob = "Bob"
-    }
-
-    getStr(){ // Just a normal func returning a string
-        const string1 = "working!!";
-        return string1;
-    }
-
-    render(){ // render method is essentially "what are we outputting?"
-        return( // Anything in {} is evaluated as js code! so string1 prints correctly
-                // we call "this" because the getStr() function is in this class/component object
-            <h1>{this.bob}, it's {this.getStr()}</h1>
-            // in js, we can do stuff like 'var div = document.createElement("div")'
-            // this allows stuff like "div.innerHTML" for example. But this is effort, so react allows us to type native HTML as above
-        );
-    }
-}
-
 const app = document.getElementById('app'); // we get the 'app' id from the html page
 ReactDOM.render(<Layout/>, app); // here's where the rendering happens. We use the class as an element tag and render it into the page id we got
---
+++ src/js/components/layout.js
@@ -0,0 +1,22 @@
+class Layout extends React.Component{ // Everything is a component, similar to everything in HTML being an element (head, body, h1 etc)
+    
+    // Here, we're gonna type some Javascript code
+    constructor(){// js has constructor methods!
+        super(); // because we're inside a component, we must always super() a constructor (inheritance basics)
+        this.bob = "Bob"
+    }
+
+    getStr(){ // Just a normal func returning a string
+        const string1 = "working!!";
+        return string1;
+    }
+
+    render(){ // render method is essentially "what are we outputting?"
+        return( // Anything in {} is evaluated as js code! so string1 prints correctly
+                // we call "this" because the getStr() function is in this class/component object
+            <h1>{this.bob}, it's {this.getStr()}</h1>
+            // in js, we can do stuff like 'var div = document.createElement("div")'
+            // this allows stuff like "div.innerHTML" for example. But this is effort, so react allows us to type native HTML as above
+        );
+    }
+}
